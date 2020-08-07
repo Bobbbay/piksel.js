c990cd13762f898f2f24bb7832be3105f6d412b5
Bobbbay <abatterysingle@gmail.com>
2020-08-07 16:23:59 -0400

    More initial code

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 class Character {
   constructor(animation_time) {
-    this.args = arguments;
     this.time = animation_time;
-    console.log(this.args);
+    this.args = arguments;
   }
 
-  render(id, posX, posY) {
+  init(id) {
+    document.getElementById(id).setAttribute("width", window.innerWidth);
+    document.getElementById(id).setAttribute("height", window.innerHeight);
+
+    this.id = id;
+  }
+
+  render(posX, posY) {
     let iteration = 1;
     let write = this.write;
     let args = this.args;
+    let id = this.id;
 
     write(id, iteration, args, posX, posY);
     if (iteration == args.length - 1) {
@@ -36,19 +43,20 @@ class Character {
 
     // Increase height
     var ih = 0;
-    // Increase length
-    var il = 0;
+
+    var extra = 0;
 
     for (var i = 0; i < args[iteration].length; i++) {
       if (!(args[iteration][i] == "\n")) {
+        extra += 1;
         // Set fillstyle to color
         ctx.fillStyle = args[iteration][i];
-        // Fill at position with a width of 10x10
-        ctx.fillRect(posX + 10 * i - il, posY + ih, 10, 10);
+        // Fill at position with a width and height of 10x10
+        ctx.fillRect(posX + 10 * extra, posY + ih, 10, 10);
       } else {
         // Is a \n, add to ih and il
         ih += 10;
-        il += 60;
+        extra = 0;
       }
     }
   }
@@ -59,13 +67,17 @@ class Background {
     this.color = color;
   }
 
-  render(id) {
+  init(id) {
+    this.id = id;
+  }
+
+  render() {
     // Initialize Canvas
-    var c = document.getElementById(id);
+    var c = document.getElementById(this.id);
     var ctx = c.getContext("2d");
 
-    let canvasWidth = document.getElementById(id).offsetWidth;
-    let canvasHeight = document.getElementById(id).offsetHeight;
+    let canvasWidth = document.getElementById(this.id).width;
+    let canvasHeight = document.getElementById(this.id).height;
 
     for (var i = 0; i < canvasWidth; i++) {
       for (var l = 0; l < canvasHeight; l++) {
